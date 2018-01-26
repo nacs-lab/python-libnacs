a278bbc5d1033d0516dc482f7ebe13b63e8b6fd4
Yichao Yu <yyc1992@gmail.com>
2018-01-25 23:08:48 -0500

    [python] use c_void_p instead of c_char_p, this disables whatever automatic convertion that python is doing that we don't want

--- libnacs/__init__.py
+++ libnacs/__init__.py
@@ -4,13 +4,13 @@ from . import handle
 import ctypes
 
 nacs_seq_bin_to_bytecode = handle.nacs_seq.nacs_seq_bin_to_bytecode
-nacs_seq_bin_to_bytecode.restype = ctypes.c_char_p
+nacs_seq_bin_to_bytecode.restype = ctypes.c_void_p
 nacs_seq_bin_to_bytecode.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_size_t,
                                      ctypes.POINTER(ctypes.c_size_t)]
 
 PyBytes_FromStringAndSize = ctypes.pythonapi['PyBytes_FromStringAndSize']
 PyBytes_FromStringAndSize.restype = ctypes.py_object
-PyBytes_FromStringAndSize.argtypes = [ctypes.c_char_p, ctypes.c_ssize_t]
+PyBytes_FromStringAndSize.argtypes = [ctypes.c_void_p, ctypes.c_ssize_t]
 
 free = handle.libc['free']
 free.restype = None
