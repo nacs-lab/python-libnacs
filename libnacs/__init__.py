4fa9700cf6dc5cb2a6153cbeb77258cab638a3ed
Yichao Yu <yyc1992@gmail.com>
2018-01-25 21:25:28 -0500

    [seq] typo...

--- libnacs/__init__.py
+++ libnacs/__init__.py
@@ -5,8 +5,8 @@ import ctypes
 
 nacs_seq_bin_to_bytecode = handle.nacs_seq.nacs_seq_bin_to_bytecode
 nacs_seq_bin_to_bytecode.restype = ctypes.c_char_p
-f.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_c_size_t,
-              ctypes.POINTER(ctypes.c_c_size_t)]
+nacs_seq_bin_to_bytecode.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_size_t,
+                                     ctypes.POINTER(ctypes.c_size_t)]
 
 PyBytes_FromStringAndSize = ctypes.pythonapi['PyBytes_FromStringAndSize']
 PyBytes_FromStringAndSize.restype = ctypes.py_object
@@ -17,7 +17,7 @@ free.restype = None
 free.argtypes = [ctypes.c_void_p]
 
 def bin_to_bytecode(bin):
-    outsize = ctypes.c_c_size_t()
+    outsize = ctypes.c_size_t()
     ptr = nacs_seq_bin_to_bytecode(ctypes.c_int.from_buffer(bin), len(bin),
                                    ctypes.byref(outsize))
     b = PyBytes_FromStringAndSize(ptr, outsize.value)
