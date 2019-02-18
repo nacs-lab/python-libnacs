d05b2dad589f51bab75585cf6d4c35db2fdc3d0c
Yichao Yu <yyc1992@gmail.com>
2019-02-18 15:51:20 -0500

    [wavemeter] python wrapper for new wavemeter parser

--- libnacs/wavemeter.py
+++ libnacs/wavemeter.py
@@ -4,43 +4,35 @@ from . import handle
 import ctypes
 import array
 
-nacs_seq_new_wavemeter_parser = handle.nacs_seq.nacs_seq_new_wavemeter_parser
-nacs_seq_new_wavemeter_parser.restype = ctypes.c_void_p
-nacs_seq_new_wavemeter_parser.argtypes = []
+nacs_utils_new_wavemeter_parser = handle.nacs_utils.nacs_utils_new_wavemeter_parser
+nacs_utils_new_wavemeter_parser.restype = ctypes.c_void_p
+nacs_utils_new_wavemeter_parser.argtypes = [ctypes.c_double, ctypes.c_double]
 
-nacs_seq_new_wavemeter_parser_withlim = handle.nacs_seq.nacs_seq_new_wavemeter_parser_withlim
-nacs_seq_new_wavemeter_parser_withlim.restype = ctypes.c_void_p
-nacs_seq_new_wavemeter_parser_withlim.argtypes = [ctypes.c_double, ctypes.c_double]
+nacs_utils_free_wavemeter_parser = handle.nacs_utils.nacs_utils_free_wavemeter_parser
+nacs_utils_free_wavemeter_parser.restype = None
+nacs_utils_free_wavemeter_parser.argtypes = [ctypes.c_void_p]
 
-nacs_seq_free_wavemeter_parser = handle.nacs_seq.nacs_seq_free_wavemeter_parser
-nacs_seq_free_wavemeter_parser.restype = None
-nacs_seq_free_wavemeter_parser.argtypes = [ctypes.c_void_p]
-
-nacs_seq_wavemeter_parse = handle.nacs_seq.nacs_seq_wavemeter_parse
-nacs_seq_wavemeter_parse.restype = ctypes.c_size_t
-nacs_seq_wavemeter_parse.argtypes = [ctypes.c_void_p, ctypes.c_char_p,
-                                     ctypes.POINTER(ctypes.c_void_p),
-                                     ctypes.POINTER(ctypes.c_void_p), ctypes.c_int]
+nacs_utils_wavemeter_parse = handle.nacs_utils.nacs_utils_wavemeter_parse
+nacs_utils_wavemeter_parse.restype = ctypes.c_size_t
+nacs_utils_wavemeter_parse.argtypes = [ctypes.c_void_p, ctypes.c_char_p,
+                                       ctypes.POINTER(ctypes.c_void_p),
+                                       ctypes.POINTER(ctypes.c_void_p),
+                                       ctypes.c_double, ctypes.c_double]
 
 PyBytes_FromStringAndSize = ctypes.pythonapi['PyBytes_FromStringAndSize']
 PyBytes_FromStringAndSize.restype = ctypes.py_object
 PyBytes_FromStringAndSize.argtypes = [ctypes.c_void_p, ctypes.c_ssize_t]
 
 class WavemeterParser:
-    def __init__(self, lo=None, hi=None):
-        if lo is None and hi is None:
-            self.hdl = nacs_seq_new_wavemeter_parser()
-        elif lo is None or hi is None:
-            raise ValueError("Lower and upper limits must be supplied at the same time")
-        else:
-            self.hdl = nacs_seq_new_wavemeter_parser_withlim(lo, hi)
+    def __init__(self, lo=0, hi=1.7976931348623157e308):
+        self.hdl = nacs_utils_new_wavemeter_parser_withlim(lo, hi)
 
-    def parse(self, name, use_cache):
+    def parse(self, name, tstart=0, tend=1.7976931348623157e308):
         ptime = ctypes.c_void_p()
         pdata = ctypes.c_void_p()
-        sz = nacs_seq_wavemeter_parse(self.hdl, name.encode(),
-                                      ctypes.byref(ptime), ctypes.byref(pdata),
-                                      use_cache)
+        sz = nacs_utils_wavemeter_parse(self.hdl, name.encode(),
+                                        ctypes.byref(ptime), ctypes.byref(pdata),
+                                        tstart, tend)
         if sz == 0:
             return array.array('d'), array.array('d')
         btime = PyBytes_FromStringAndSize(ptime, sz * 8)
@@ -48,4 +40,4 @@ class WavemeterParser:
         return array.array('d', btime), array.array('d', bdata)
 
     def __del__(self):
-        nacs_seq_free_wavemeter_parser(self.hdl)
+        nacs_utils_free_wavemeter_parser(self.hdl)
