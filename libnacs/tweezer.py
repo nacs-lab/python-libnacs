6f2f55005bb4126e21e49a2cd35348ab9c435989
Yichao Yu <yyc1992@gmail.com>
2019-06-10 18:23:56 -0400

    [tweezer] python API for sideband strength

--- libnacs/tweezer.py
+++ libnacs/tweezer.py
@@ -9,7 +9,14 @@ nacs_tweezer_max_sideband_ratios.restype = None
 nacs_tweezer_max_sideband_ratios.argtypes = [ctypes.c_double, ctypes.c_int,
                                              ctypes.POINTER(ctypes.c_double)]
 
+nacs_tweezer_sideband = handle.nacs_utils.nacs_tweezer_sideband
+nacs_tweezer_sideband.restype = ctypes.c_double
+nacs_tweezer_sideband.argtypes = [ctypes.c_double, ctypes.c_int, ctypes.c_int]
+
 def max_sideband_ratios(eta, orders):
     ptr = (ctypes.c_double * orders)()
     nacs_tweezer_max_sideband_ratios(eta, orders, ptr)
     return array.array('d', ptr)
+
+def sideband(eta, n1, n2):
+    return nacs_tweezer_sideband(eta, n1, n2)
